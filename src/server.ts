f01f0831660f5d1336e82d739abf59e3a7fa2b96
Thiago Lago <thiago.lago@tips4y.pt>
2024-07-03 15:20:15 -0300

    :sparkles: add zod, dotenv and sqlite3. Make the initial migrations test

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,27 @@
 import fastify from 'fastify';
 import { knex } from './database';
+import crypto from 'node:crypto';
+import { env } from './env';
 
 const app = fastify();
 
 app.get('/hello', async () => {
-  const testTables = await knex('sqlite_schema').select('*');
-  return testTables;
+  // const transaction = await knex("transactions").insert({
+  //   id: crypto.randomUUID(),
+  //   title: "Transação de teste",
+  //   amount: 1000
+  // })
+  // .returning("*")
+
+  // return transaction
+
+  // const transactions = await knex("transactions").select("*");
+  // return transactions;
+
+  const transactions = await knex("transactions").where("amount", 1000).select("*")
+  return transactions;
 });
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP Server Running!')
 });
